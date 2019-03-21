761ca20931b00de648256a5cdc7fc6d014e26bcf
Christian Luã Lemos <christianlualemosc@hotmail.com>
2019-03-21 07:37:01 -0300

    Minimap Controller

---
+++ Assets/Scripts/Player/MinimapController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapController : MonoBehaviour
+{
+    public ICollection<GameObject> visibleObjects = new HashSet<GameObject>();
+
+    private void LateUpdate()
+    {
+        ICollection<GameObject> visibleNow = MapObjecsRenderingController.Instance.visibleObjects;
+
+        foreach (GameObject gameObject in visibleObjects)
+        {
+            if (gameObject != null && !visibleNow.Contains(gameObject))
+            {
+                SetRender(false, gameObject);
+            }
+        }
+
+        foreach (GameObject gameObject in visibleNow)
+        {
+            if (!visibleObjects.Contains(gameObject))
+            {
+                SetRender(true, gameObject);
+            }
+        }
+
+        visibleObjects = visibleNow;
+    }
+
+    private void SetRender(bool value, GameObject gameObject)
+    {
+        MiniMapIcon miniMapIcon = gameObject.GetComponentInChildren<MiniMapIcon>();
+
+        if (miniMapIcon != null)
+        {
+            if (value)
+            {
+                Color color = Color.red;
+                int goPlayer = PlayerDatabase.Instance.GetObjectPlayer(gameObject);
+                for (int i = 0; i < MapObjecsRenderingController.Instance.players.Length; i++)
+                {
+                    if (goPlayer == MapObjecsRenderingController.Instance.players[i])
+                    {
+                        color = Color.blue;
+                        break;
+                    }
+                }
+                miniMapIcon.GetComponent<SpriteRenderer>().color = color;
+                miniMapIcon.GetComponent<SpriteRenderer>().enabled = true;
+            }
+            else
+            {
+                miniMapIcon.GetComponent<SpriteRenderer>().enabled = false;
+            }
+        }
+    }
+}
