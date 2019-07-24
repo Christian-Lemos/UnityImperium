209b2f201cd633e6e9181f3f50a2e9b39c6ba023
Christian Luã Lemos <christianlualemosc@hotmail.com>
2019-07-23 23:58:48 -0300

    PhotonNetwork added

---
+++ Assets/Lib/Persistance/BinarySerializer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Lib.Persistance
+{
+    public static class BinarySerializer
+    {
+        private static readonly BinaryFormatter binaryFormatter = new BinaryFormatter();
+        public static byte[] BinarySerialization(this object @object)
+        {
+            MemoryStream memoryStream = new MemoryStream();
+            binaryFormatter.Serialize(memoryStream, @object);
+            memoryStream.Close();
+            return memoryStream.ToArray();
+        }
+
+        public static object BinaryDeserialization<T>(this byte[] byteArray)
+        {
+            MemoryStream memoryStream = new MemoryStream(byteArray);
+            T @object = (T)binaryFormatter.Deserialize(memoryStream);
+            memoryStream.Close();
+            return @object;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Lobby/LoginPanelController.cs
@@ -0,0 +1,111 @@
+﻿using Photon.Pun;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.UI.Lobby
+{
+    public class LoginPanelController : MonoBehaviourPunCallbacks
+    {
+        [SerializeField]
+        private Image background;
+
+        [SerializeField]
+        private Button loginButton;
+
+        [SerializeField]
+        private InputField loginInputField;
+
+        [SerializeField]
+        private Text statusText;
+
+        private void Awake()
+        {
+            background = GetComponent<Image>();
+            ShowPanel();
+        }
+
+        #region UNITY
+
+        private void Start()
+        {
+            loginButton.onClick.AddListener(OnLoginButtonClicked);
+        }
+
+        #endregion UNITY
+
+        #region UI CALLBACKS
+
+        private void OnLoginButtonClicked()
+        {
+            string nickname = loginInputField.text;
+            if (nickname.Equals(""))
+            {
+                nickname = "Player 1";
+            }
+
+            PhotonNetwork.LocalPlayer.NickName = nickname;
+
+            PhotonNetwork.ConnectUsingSettings();
+            SetStatusText("Connecting to server...");
+        }
+
+        #endregion UI CALLBACKS
+
+        #region PUN
+
+        public override void OnConnectedToMaster()
+        {
+            if(!PhotonNetwork.InLobby)
+            {
+                SetStatusText("Joining server's lobby");
+                PhotonNetwork.JoinLobby();
+            }
+            else
+            {
+                OnJoinedLobby();
+            }
+        }
+
+        public override void OnJoinedLobby()
+        {
+            SetStatusText("");
+            HidePanel();
+        }
+
+        #endregion PUN
+
+        private void HidePanel()
+        {
+            /*Text[] texts = GetComponentsInChildren<Text>();
+            foreach(Text text in texts)
+            {
+                text.gameObject.SetActive(false);
+            }
+
+            background.gameObject.SetActive(false);
+            loginButton.gameObject.SetActive(false);
+            loginInputField.gameObject.SetActive(false);*/
+
+            foreach (Transform child in this.transform)
+            {
+                child.gameObject.SetActive(false);
+            }
+            background.enabled = false;
+        }
+
+        private void ShowPanel()
+        {
+            foreach (Transform child in this.transform)
+            {
+                child.gameObject.SetActive(true);
+            }
+            background.enabled = true;
+        }
+
+        private void SetStatusText(string text)
+        {
+            statusText.text = text;
+            statusText.enabled = !text.Equals("");
+        }
+    }
+}
